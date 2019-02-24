3be766906e79daf0a1195bc73a0987308adb60a6
SukhachevN <traed.traed@mail.ru>
2019-02-24 17:35:12 +0300

    Добавлены методы:

---
+++ train_schedule/src/train_schedule/schedule.java
@@ -0,0 +1,67 @@
+package train_schedule;
+
+import java.util.LinkedList;
+import java.util.Map;
+
+public class schedule {
+	public Map<String, LinkedList<String>> allTrains; //расписание поездов имя
+	public schedule( Map<String, LinkedList<String>> allTrains) {
+		this.allTrains = allTrains;
+	}
+	public void addTrain(String name , String endStation , String time) {  // Добавление нового поезда, необходимо сразу указывать название поезда и конечную станцию
+		if (!allTrains.containsKey(name)) {
+			LinkedList<String> trainInfo = new LinkedList<String>();
+			trainInfo.add(time);// первый элемент списка
+			trainInfo.add(endStation); // последний элемент списка, который будет смещаться при добавлении новых станций(объяснение в addStation), и всегда будет последним
+			allTrains.put(name , trainInfo);
+			System.out.println("Поезд добавлен");
+		}
+		else {
+			System.out.println("Ошибка, поезд уже существует");
+		}
+	}
+	
+	public void deleteTrain(String name) { //удаление поезда
+		if (allTrains.containsKey(name)) {
+			allTrains.remove(name);
+			System.out.println("Поезд удален");
+		}
+		else {
+			System.out.println("Ошибка, такого поезда не существует");
+		}
+		
+	}
+	public void addStation(String name , String Station) { //добавление промежуточных станций
+		if (allTrains.containsKey(name)) {
+			if (!(allTrains.get(name).contains(Station))){
+				allTrains.get(name).add(1, Station);//добавляет новую станцию, при этом конечная станция остается последней в списке
+				System.out.println("Станция добавлена");
+			}
+			else {
+				System.out.println("Такая станция уже добавлена");
+			}
+		}
+		else {
+			System.out.println("Ошибка, такого поезда не существует");
+		}
+	}
+	 public void deleteStation(String name, String Station) { // удаление промежуточной станции
+		 if (allTrains.containsKey(name)) {
+			 if (allTrains.get(name).contains(Station)){
+				 if (!Station.equals(allTrains.get(name).getLast())) {
+					 allTrains.get(name).remove(Station);
+				 }
+				 else {
+					 System.out.println("Ошибка, нельзя удалить конечную станцию");
+				 }
+			 }
+			 else {
+				 System.out.println("Ошибка, такой станции не существует");
+			 }
+		 }
+		 else {
+			 System.out.println("Ошибка, такого поезда не существует");
+		 }
+	 }
+
+}
